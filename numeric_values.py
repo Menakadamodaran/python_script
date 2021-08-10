4b9cf137240e460850d120d880f2d8668e18fb16
Menakadamodaran <menakadamodharan04@gmail.com>
2021-08-10 11:35:25 -0400

    Script added for numeric values

---
+++ numeric_values.py
@@ -0,0 +1,8 @@
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of data type " + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of data type " + str(type(myValue)))
