4a42f40635182d90593fa93eaf5b55ac0cf90035
Menakadamodaran <menakadamodharan04@gmail.com>
2021-08-10 14:52:39 -0400

    Added more

--- numeric_values.py
+++ numeric_values.py
@@ -6,3 +6,15 @@ myValue=3.14
 print(myValue)
 print(type(myValue))
 print(str(myValue) + " is of data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of data type " + str(type(myValue)))
