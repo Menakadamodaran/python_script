145f2b18d637e79a70e0981812fd02afaa7e4d2a
Menakadamodaran <menakadamodharan04@gmail.com>
2021-08-11 11:22:12 -0400

    created list

---
+++ list_tuple_dictionary.py
@@ -0,0 +1,21 @@
+myFruits= ["apple" , "banana" , "cherry"]
+print(myFruits)
+print(type(myFruits))
+print(myFruits[0])
+print(myFruits[1])
+print(myFruits[2])
+myFruits[2]= "orange"
+print(myFruits)
+myTuple= ("apple","banana","pineapple")
+print(myTuple)
+print(type(myTuple))
+print(myTuple[0])
+print(myTuple[1])
+print(myTuple[2])
+myDictionary = {'Adam' : 'apple' ,'Ben' : 'banana',
+'Penny' : 'Pineapple'}
+print(myDictionary)
+print(type(myDictionary))
+print(myDictionary["Adam"])
+print(myDictionary["Ben"])
+print(myDictionary["Penny"])
